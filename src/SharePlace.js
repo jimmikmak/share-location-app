1d08c0b0a131d4ec2600c1da28840f170cf1f049
James McCarron <mccarron.j@gmail.com>
2021-09-29 12:38:03 +1000

    Get the user location

--- src/SharePlace.js
+++ src/SharePlace.js
@@ -7,7 +7,30 @@ class PlaceFinder {
     addressForm.addEventListener("submit", this.findAddressHandler);
   }
 
-  locateUserHandler() {}
+  locateUserHandler() {
+    if (!navigator.geolocation) {
+      alert(
+        "Location feature is not available in your browser - please use a more modern browser or manually enter an address."
+      );
+      return;
+    }
+    navigator.geolocation.getCurrentPosition(
+      (successResult) => {
+        const coordinates = {
+          lat: successResult.coords.latitude,
+          lng: successResult.coords.longitude,
+        };
+        console.log(coordinates);
+      },
+      (error) => {
+        alert(
+          "Could not locate you unfortunately. Please enter an address manually!"
+        );
+      }
+    );
+  }
 
   findAddressHandler() {}
 }
+
+const placeFinder = new PlaceFinder();
