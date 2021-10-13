4bebc3e30226266801c7142da20cc44e4e7e2801
James McCarron <mccarron.j@gmail.com>
2021-10-13 11:22:38 +1100

    Changed the selectPlace shared link data address

--- src/SharePlace.js
+++ src/SharePlace.js
@@ -52,15 +52,11 @@ class PlaceFinder {
         return response.json();
       })
       .then((data) => {
-        console.log(data);
+        const locationId = data.locId;
+        this.shareBtn.disabled = false;
+        const sharedLinkInputElement = document.getElementById("share-link");
+        sharedLinkInputElement.value = `${location.origin}/my-place?location=${locationId}`;
       });
-    this.shareBtn.disabled = false;
-    const sharedLinkInputElement = document.getElementById("share-link");
-    sharedLinkInputElement.value = `${
-      location.origin
-    }/my-place?address=${encodeURI(address)}&lat=${coordinates.lat}&lng=${
-      coordinates.lng
-    }`;
   }
 
   locateUserHandler() {
